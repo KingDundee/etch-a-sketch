ab7fc1eb077399d9dd2498892a84bc4ca92984e7
KingDundee <118585837+KingDundee@users.noreply.github.com>
2023-01-24 21:03:56 -0800

    Define createGrid function

---
+++ scriptMain.js
@@ -0,0 +1,21 @@
+const body = document.querySelector('body');
+const container = document.createElement('div');
+container.classList.add('container');
+body.appendChild(container);
+
+// create 16x16 grid of square divs
+
+function createGrid(numberX, numberY, width, height) {
+  for (let i = 1; i <= numberY; i++) {
+    for (let j = 1; j <= numberX + 1; j++) {
+      let divTemp = document.createElement('div');
+      if (j === 1) {
+        divTemp.style.cssText = 'clear: left;';  // row separator
+      }
+      else {
+        divTemp.style.cssText = `float: left; width: ${width}px; height: ${height}px; background-color: red; border: 2px solid black;`;
+      }
+      container.appendChild(divTemp);
+    }
+  }
+}
