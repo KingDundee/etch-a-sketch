5fe8fafb805bfcb0dc22c73fd70058cfad7cb7c4
KingDundee <118585837+KingDundee@users.noreply.github.com>
2023-01-26 20:47:29 -0800

    Add new functions and UI elements

--- scriptMain.js
+++ scriptMain.js
@@ -4,19 +4,39 @@ const container = document.createElement('div');
 container.classList.add('container');
 body.insertBefore(container, footer);
 
-function createGrid(numberX, numberY, width, height) {
-  for (let i = 1; i <= numberY; i++) {
-    for (let j = 1; j <= numberX + 1; j++) {
-      let divTemp = document.createElement('div');
+function createGrid(numberPerSide) {
+  for (let i = 1; i <= numberPerSide; i++) {
+    for (let j = 1; j <= numberPerSide + 1; j++) {
+      let divGrid = document.createElement('div');
       if (j === 1) {
-        divTemp.classList.add('row-separator');
-        divTemp.style.cssText = 'clear: left;';
+        divGrid.classList.add('row-separator');
+        divGrid.style.cssText = 'clear: left;';
       }
       else {
-        divTemp.classList.add('grid');
-        divTemp.style.cssText = `float: left; width: ${width}px; height: ${height}px;`;
+        divGrid.classList.add('grid-unit');
+        divGrid.style.cssText = `float: left; 
+                                 width: ${container.offsetWidth / numberPerSide}px; 
+                                 height: ${container.offsetHeight / numberPerSide}px;`;
       }
-      container.appendChild(divTemp);
+      container.appendChild(divGrid);
     }
   }
+}
+
+const submitButton = document.querySelector('#submit');
+submitButton.addEventListener('click', getResolution);
+
+function getResolution() {
+  clearGrid();
+  if (document.getElementById('num-per-side').value >= 1 && document.getElementById('num-per-side').value <= 20) {
+    createGrid(document.getElementById('num-per-side').value);
+  }
+  else {
+    alert('Please enter a resolution between 1 and 20!');
+  }
+}
+
+function clearGrid() {
+  document.querySelectorAll('.row-separator').forEach((el) => {el.remove()});
+  document.querySelectorAll('.grid-unit').forEach((el) => {el.remove()});
 }
